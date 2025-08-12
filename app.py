08a528bc2b9708d031bea84b571b7acc2057bc6c
Graus-GH <merch@graus.bz.it>
2025-08-12 13:40:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+
+
+
 import io
 import re
 from typing import List, Tuple
@@ -244,7 +247,7 @@ with st.sidebar:
             placeholder="Es. 'riesling alto adige 0,75'",
         )
         st.checkbox(
-            "Includi FINE WINES\n⏳ disponibilità salvo conferma e consegna minimo 3 settimane.",
+            "Includi FINE WINES (⏳)\nDisponibilità salvo conferma e consegna minimo 3 settimane.",
             value=st.session_state.include_fw,
             key="include_fw",
         )
@@ -444,3 +447,8 @@ if st.session_state.active_tab == "Prodotti":
             st.rerun()
         else:
             st.info("Seleziona almeno un articolo dal paniere.")
+
+
+
+
+
